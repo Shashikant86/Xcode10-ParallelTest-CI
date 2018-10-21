5189ba7886d7b8063bdc5ace33ec2e3cd0a0c10a
Shashikant86 <shashikant.jagtap@icloud.com>
2018-10-21 11:17:59 +0100

    Initial Commit

---
+++ Xcode10-ParallelTest-CI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xcode10-ParallelTest-CI
+//
+//  Created by Shashikant Jagtap on 21/10/2018.
+//  Copyright © 2018 Shashikant Jagtap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
