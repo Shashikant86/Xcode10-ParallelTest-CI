83a86650fe1d7fb31c2edd893a11ba454ccabd1d
Shashikant86 <shashikant.jagtap@icloud.com>
2018-10-21 11:26:39 +0100

    parallel testing demo Travis

---
+++ Xcode10-ParallelTest-CIUITests/UITest2.swift
@@ -0,0 +1,32 @@
+//
+//  UITest2.swift
+//  Xcode10-ParallelTest-CIUITests
+//
+//  Created by Shashikant Jagtap on 21/10/2018.
+//  Copyright © 2018 Shashikant Jagtap. All rights reserved.
+//
+
+import XCTest
+
+class UITest2: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test2AssertMe() {
+        XCTAssertTrue(XCUIApplication().buttons["AssertMe"].exists)
+    }
+}
